ab451cbd5fcb74ea9ebc1c08ad557e7acb76c888
zalinadzh <z.dzhambotova@mail.ru>
2026-02-10 20:30:17 +0300

    Practice JS variables and operators

---
+++ script.js
@@ -0,0 +1,17 @@
+                       let nApples = 22;
+                       let cPerApple = 52;
+                       let tCalories = nApples + cPerApple;
+                      console.log(tCalories);
+
+
+                      let nameRoll = "Philadelphy"
+                      let porciaOne = 550;
+                      let clientPorcia = 2;
+                      let totalPrice = porciaOne * clientPorcia;
+                      console.log("Заказ: " + nameRoll + ", итоговая сумма: " + totalPrice + " рублей");
+
+
+                      let discount = 100;
+                      totalPrice = totalPrice - discount;
+                      console.log("Цена со скидкой: " + totalPrice + " рублей")
+
