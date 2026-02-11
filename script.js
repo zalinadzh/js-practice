6ee66a4b173ae6e29d6f7e689b5e5b1e4499daad
zalinadzh <z.dzhambotova@mail.ru>
2026-02-11 22:17:47 +0300

    Variables and operators practice

--- script.js
+++ script.js
@@ -98,6 +98,28 @@
                        let score = 10;
                        score += 5;
                        console.log(score);
+
+
+                       let d = 10;
+                       let f =20;
+                       d = d + f;
+                       f = d - f;
+                       d = d - f;
+                       console.log(d, f);
+                         
+
+                    
+                       let side = 8;
+                       let perimeter = side * 4;
+                       console.log(perimeter);
+
+
+                       let num1 = 10;
+                       let num2 = 20;
+                       let num3 = 30;
+                       let average = (num1 + num2 + num3) / 3;
+                       console.log(average);
+
                        
 
 
